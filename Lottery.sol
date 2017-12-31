59f79417bff0a3fe655c19505e3b28996d28b5cc
Sean Wilkinson <swilkinson@smarttermlms.com>
2017-12-30 20:59:46 -0500

    Add payable function to accept bets from users

--- Lottery.sol
+++ Lottery.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.4.17;
 
 contract Lottery {
-    
+    mapping (address=>uint) userBets;
+    mapping (uint=>address) users;
+    uint totalUsers = 0;
+    uint totalBets = 0;
+    address owner;
+
+    function bet() public payable {
+        if (msg.value > 0) {
+            if (userBets[msg.sender] == 0) {
+                users[totalUsers] = msg.sender;
+                totalUsers += 1;
+            }
+            userBets[msg.sender] += msg.value;
+            totalBets += msg.value;
+        }
+    }
 }
